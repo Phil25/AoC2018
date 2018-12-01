b5754c7f7d12a6c731347e1d62fe1bd188aeb571
Phil25 <Phil25@ProtonMail.com>
2018-12-01 10:16:47 +0100

    Switched to set

--- 1/Main.hs
+++ 1/Main.hs
@@ -1,4 +1,5 @@
-import System.Environment
+import           System.Environment
+import qualified Data.Set as Set
 
 --- Part One ---
 readFreq :: String -> Int
@@ -9,10 +10,10 @@ getFreq :: String -> [Int]
 getFreq = map readFreq . lines
 
 --- Part Two ---
-findDup :: [Int] -> [Int] -> Int -> Int
-findDup (x:xs) freqHistory acc
-    | acc `elem` freqHistory = acc
-    | otherwise              = findDup xs (acc:freqHistory) (acc+x)
+findDup :: [Int] -> (Set.Set Int) -> Int -> Int
+findDup (x:xs) freqSet acc
+    | acc `Set.member` freqSet = acc
+    | otherwise                = findDup xs (acc `Set.insert` freqSet) (acc+x)
 
 main :: IO ()
 main = do
@@ -20,4 +21,4 @@ main = do
     content <- readFile (args !! 0)
     let freqList = getFreq content
     print $ sum freqList
-    print $ findDup (cycle freqList) [] 0
+    print $ findDup (cycle freqList) Set.empty 0
