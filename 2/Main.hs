6c0aa55a2ae740ede711ea947f4caeff29179add
Phil25 <Phil25@ProtonMail.com>
2018-12-02 08:19:09 +0100

    Day 2

---
+++ 2/Main.hs
@@ -0,0 +1,44 @@
+import           System.Environment
+import qualified Data.Map as Map
+
+--- Part One ---
+getFreq :: String -> [Int]
+getFreq xs = Map.elems $ Map.fromListWith (+) [(x, 1) | x <- xs]
+
+countExacts :: (Int, Int) -> String -> (Int, Int)
+countExacts (ex2, ex3) val
+    | has2 && has3 = (ex2+1, ex3+1)
+    | has2         = (ex2+1, ex3)
+    | has3         = (ex2, ex3+1)
+    | otherwise    = (ex2, ex3)
+        where frequency = getFreq val
+              has2 = 2 `elem` frequency
+              has3 = 3 `elem` frequency
+
+getChecksum :: [String] -> Int
+getChecksum contents = ex2 * ex3
+    where (ex2, ex3) = foldl countExacts (0, 0) contents
+
+--- Part Two ---
+getDiff :: String -> String -> Int
+getDiff (x:[]) (y:_)  = 0
+getDiff (x:xs) (y:ys)
+    | x /= y    = getDiff xs ys + 1
+    | otherwise = getDiff xs ys
+
+getOneCharDiff :: [(String, String)] -> (String, String)
+getOneCharDiff (_:[]) = ("", "")
+getOneCharDiff ((s1,s2):xs)
+    | getDiff s1 s2 == 1 = (s1, s2)
+    | otherwise          = getOneCharDiff xs
+
+main :: IO ()
+main = do
+    args    <- getArgs
+    content <- readFile (args !! 0)
+    let testset = lines content
+        product = [(x,y) | x <- testset, y <- testset]
+        result  = getOneCharDiff $ product
+    print $ getChecksum testset
+    print $ fst result
+    print $ snd result
