762bb5f4af6e2ea4ab211f8d262461d3c2b82abe
Phil25 <Phil25@ProtonMail.com>
2018-12-03 21:35:42 +0100

    Day 3 Part 2

--- 3/Main.hs
+++ 3/Main.hs
@@ -2,16 +2,25 @@ import           System.Environment
 import           Data.List
 import qualified Data.Set as Set
 
+{-
+ - FAIR WARNING:
+ - This code is fucking awful, I'm ashamed that it works
+ - YOU HAVE BEEN WARNED
+-}
+
 type Point = (Int,Int)
-type Rect  = (Int,Int,Int,Int)
+type Rect  = (Int,Int,Int,Int,Int) -- id, x, y, width, height
+
+combinations :: [a] -> [(a,a)]
+combinations xs = [(x,y) | (x:rest) <- tails xs, y <- rest]
 
 --- Part One ---
 toRect :: String -> Rect
-toRect rect = (read x, read y, read w, read h)
-    where (x:y:w:h:[]) = words rect
+toRect rect = (read i, read x, read y, read w, read h)
+    where (i:x:y:w:h:[]) = words rect
 
 getRectPoints :: Rect -> [Point]
-getRectPoints (x,y,w,h) = [(x',y') | x' <- [x..x+w-1], y' <- [y..y+h-1]]
+getRectPoints (_,x,y,w,h) = [(x',y') | x' <- [x..x+w-1], y' <- [y..y+h-1]]
 
 getOverlapPoints :: (Rect,Rect) -> Set.Set Point
 getOverlapPoints (r1,r2) = Set.fromList ps1 `Set.intersection` Set.fromList ps2
@@ -19,9 +28,25 @@ getOverlapPoints (r1,r2) = Set.fromList ps1 `Set.intersection` Set.fromList ps2
           ps2 = getRectPoints r2
 
 getOverlapArea :: [Rect] -> Int
-getOverlapArea rects   = Set.size $ foldl appendOverlaps Set.empty combinations
-    where combinations = [(x,y) | (x:rest) <- tails rects, y <- rest]
-          appendOverlaps acc rects = Set.union acc $ getOverlapPoints rects
+getOverlapArea rects = Set.size $ foldl appendOverlaps Set.empty (combinations rects)
+    where appendOverlaps acc rects = Set.union acc $ getOverlapPoints rects
+
+--- Part Two ---
+getId :: Rect -> Int
+getId (i,_,_,_,_) = i
+
+overlaps :: Rect -> [Rect] -> Bool
+overlaps _ [] = False
+overlaps r1 (r2:rest)
+    | getId r1 == getId r2                       = overlaps r1 rest
+    | (Set.size $ getOverlapPoints (r1,r2)) /= 0 = True
+    | otherwise                                  = overlaps r1 rest
+
+getNoOverlap :: [Rect] -> [Rect] -> Rect
+getNoOverlap [] _ = (0,0,0,0,0)
+getNoOverlap (r:rs) all
+    | overlaps r all = getNoOverlap rs all
+    | otherwise      = r
 
 main :: IO ()
 main = do
@@ -29,3 +54,4 @@ main = do
     content <- readFile (args !! 0)
     let rects = map toRect $ lines content
     print $ getOverlapArea $ rects
+    print $ getId $ getNoOverlap rects rects
