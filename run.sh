a3f2749d98c0797a89d9512922e672691a957790
Phil25 <Phil25@ProtonMail.com>
2018-12-05 01:21:02 +0100

    Binary removal

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 EXT=`dirname "$1"`
 FILENAME=`basename -- "$1"`
 FILENAME="${FILENAME%.*}"
-rm $EXT/$FILENAME
+rm ./bin/$FILENAME
 
 ./compile.sh "$1" $FILENAME $EXT
 ./bin/$FILENAME ./data/$FILENAME.txt
