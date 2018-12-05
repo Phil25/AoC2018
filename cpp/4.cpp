60d05b682f3e469a1ff6ab63f9bd0be4acf6e982
Phil25 <Phil25@ProtonMail.com>
2018-12-05 15:44:19 +0100

    Day 4 part 1

---
+++ cpp/4.cpp
@@ -0,0 +1,172 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+
+enum EventType{
+	BeginShift, FallAsleep, WakeUp
+};
+
+int dayInMo[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+
+class event{
+	int timestamp = 0; // number of minutes since start of year
+	int minute = 0;
+	int guard = -1;
+	EventType type = BeginShift;
+
+	EventType getEventType(const std::string& what){
+		if(what[1] == 'G')
+			return BeginShift;
+		if(what[1] == 'f')
+			return FallAsleep;
+		return WakeUp;
+	}
+
+	int parseGuard(const std::string& what){
+		std::istringstream iss(what);
+		std::string word;
+		iss >> word >> word; // extract "Guard" then "#<num>"
+		return std::stoi(word.substr(1));
+	}
+
+	int getPassedDays(int mo){
+		int acc = 0;
+		while(--mo >= 0)
+			acc += dayInMo[mo];
+		return acc;
+	}
+
+	int makeTimestamp(int mo, int d, int h, int m){
+		return m + h*60 + d*24*60 + this->getPassedDays(mo)*24*60;
+	}
+
+public:
+	event(int mo, int d, int h, int m, const std::string& what){
+		this->timestamp = this->makeTimestamp(mo, d, h, m);
+		this->minute = m;
+		this->type = this->getEventType(what);
+
+		if(this->type == BeginShift)
+			this->guard = this->parseGuard(what);
+	}
+
+	int getTimestamp(){
+		return this->timestamp;
+	}
+
+	int getGuard(){
+		return this->guard;
+	}
+
+	EventType getType(){
+		return this->type;
+	}
+
+	int getMinute(){
+		return this->minute;
+	}
+};
+
+void getEvents(const std::string& data, std::map<int, event*>& events){
+	std::ifstream contents(data);
+	int mo, d, h, m;
+	std::string what;
+	event* e = nullptr;
+
+	while(contents >> mo >> d >> h >> m, std::getline(contents, what)){
+		e = new event(mo, d, h, m, what);
+		events.insert(std::make_pair(e->getTimestamp(), e));
+	}
+}
+
+void getSleepMinutes(std::map<int, std::vector<std::vector<int>*>>& sleepMinutes, const std::map<int, event*>& events){
+	int guard = -1, beganSleep = 0;
+	for(auto const& e : events){
+		switch(e.second->getType()){
+			case BeginShift:
+				guard = e.second->getGuard();
+				break;
+
+			case FallAsleep:
+				beganSleep = e.second->getMinute();
+				break;
+
+			case WakeUp:
+				int wokenUp = e.second->getMinute();
+				std::vector<int>* minuteVec = new std::vector<int>();
+				while(--wokenUp >= beganSleep)
+					minuteVec->push_back(wokenUp);
+
+				if(sleepMinutes.find(guard) == sleepMinutes.end())
+					sleepMinutes.emplace(guard, std::vector<std::vector<int>*>());
+
+				sleepMinutes.at(guard).push_back(minuteVec);
+				break;
+		}
+	}
+}
+
+int countSleepMinutes(std::vector<std::vector<int>*> sleepMinutes){
+	int acc = 0;
+	for(auto const& m : sleepMinutes)
+		acc += m->size();
+	return acc;
+}
+
+int getSleepmostGuard(std::map<int, std::vector<std::vector<int>*>>& sleepMinutes){
+	int guard = -1, longestSleep = 0;;
+	for(auto const& sleep : sleepMinutes){
+		int thisSleepMinutes = countSleepMinutes(sleep.second);
+		if(thisSleepMinutes <= longestSleep)
+			continue;
+		guard = sleep.first;
+		longestSleep = thisSleepMinutes;
+	}
+	return guard;
+}
+
+int getSleepmostMinute(int guard, std::map<int, std::vector<std::vector<int>*>>& sleepMinutes){
+	int minutesCount[60]{0};
+	std::vector<std::vector<int>*> guardMinutes = sleepMinutes.at(guard);
+
+	for(auto const& minuteVec : guardMinutes)
+		for(auto const& minute : *minuteVec)
+			minutesCount[minute]++;
+
+	int sleepmostMinute = 0;
+	for(int i = 0; i < 60; ++i){
+		if(minutesCount[i] < minutesCount[sleepmostMinute])
+			continue;
+		sleepmostMinute = i;
+	}
+
+	return sleepmostMinute;
+}
+
+void part1(std::string data){
+	std::map<int, event*> events = std::map<int, event*>();
+	getEvents(data, events);
+
+	std::map<int, std::vector<std::vector<int>*>> sleepMinutes;
+	getSleepMinutes(sleepMinutes, events);
+
+	int guard = getSleepmostGuard(sleepMinutes);
+	std::cout << "Sleepmost guard: " << guard << std::endl;
+
+	int minute = getSleepmostMinute(guard, sleepMinutes);
+	std::cout << "Sleepmost minute: " << minute << std::endl;
+	std::cout << "Result: " << guard * minute << std::endl;
+}
+
+int main(int argc, char* argv[]){
+	if(argc < 2){
+		std::cerr << "No data provided." << std::endl;
+		return 1;
+	}
+
+	part1(argv[1]);
+	return 0;
+}
